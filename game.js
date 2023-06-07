51a10e25ec2c18e937fe40b49a3c2c2bd9781dbe
aridaman8 <109961155+aridaman8@users.noreply.github.com>
2023-06-08 02:23:28 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
 var r1=Math.random()*6;
 r1=Math.floor(r1);
 r1++;
-var img1="images/dice"+r1+".png";
+var img1="dice"+r1+".png";
 var dicee1=document.querySelectorAll("img")[0];
 dicee1.setAttribute("src",img1);
 
 var r2=Math.random()*6;
 r2=Math.floor(r2);
 r2++;
-var img2="images/dice"+r2+".png";
+var img2="dice"+r2+".png";
 var dicee2=document.querySelectorAll("img")[1];
 dicee2.setAttribute("src",img2);
 
